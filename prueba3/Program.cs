fea639952b956ba88fbfd37ec4723af83b26cc11
GustaCortez <melvincortey@gmail.com>
2022-02-25 21:43:43 -0600

    inicio en cshare

---
+++ prueba3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace prueba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos numeros");
+            Console.Write("Ingresa numero 1: ");
+            string dato = Console.ReadLine();
+            int n1 = int.Parse(dato);
+            Console.Write("Ingrese numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es: {0}", suma);
+        }
+    }
+}
